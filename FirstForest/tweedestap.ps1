12341a018be529a800b755ed3503374d361fd1f3
CoenHo <Coen@hodzelmans.com>
2022-06-21 15:05:13 +0200

    Verder gegaan

--- FirstForest/tweedestap.ps1
+++ FirstForest/tweedestap.ps1
@@ -5,8 +5,9 @@ Install-WindowsFeature ad-domain-services, dhcp -IncludeManagementTools
 Install-ADDSForest -DomainName TCR-CoeHod.int -ForestMode Win2012R2 -DomainMode Win2012R2
 #als je dit commando uitvoert krijg je een prompt voor het safemodepassword
 
+#als je dit niet wil kan je onderstaande gebruiken
 #Region Optioneel
-    #als je dit niet wil kan je onderstaande gebruiken
+    
     $password = ConvertTo-SecureString "P@ssw0rd" -AsPlainText -Force
     $Cred = New-Object System.Management.Automation.PSCredential ("username", $password)
 
--- Opdracht 7/tweedestap.ps1
+++ Opdracht 7/tweedestap.ps1
@@ -1,2 +1,18 @@
 #nu gaan we de rollen instaleren die we nodig hebben
 Install-WindowsFeature ad-domain-services, dhcp -IncludeManagementTools
+
+#nu gaan we de domain administrator credentials opslaan in een variabele
+$cred = get-credential tcr-coehod\administrator
+
+#en dan gaan we de domaincontroller instalern
+Install-ADDSDomainController -DomainName tcr-coehod.int -credential $cred
+#er komt een prompt voor safemodeadministratorpassword
+
+#als je dit niet wil kan je onderstaande gebruiken
+#Region Optioneel
+    
+    $password = ConvertTo-SecureString "P@ssw0rd" -AsPlainText -Force
+    $smp = New-Object System.Management.Automation.PSCredential ("username", $password)
+
+    Install-ADDSDomainController -DomainName tcr-coehod.int -credential $cred -SafeModeAdministratorPassword $smp.pasword
+#EndRegion
